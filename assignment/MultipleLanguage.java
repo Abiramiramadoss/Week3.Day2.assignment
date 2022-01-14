46046d32aef7d5d6a8a33e6a416c9f1601cab019
Abiramiramadoss <96653010+Abiramiramadoss@users.noreply.github.com>
2022-01-14 22:54:59 +0530

    Add files via upload

---
+++ assignment/MultipleLanguage.java
@@ -0,0 +1,12 @@
+package week3.day2.assignment;
+
+public abstract class MultipleLanguage {
+	public void python() {
+		System.out.println("known language");
+		
+
+	}
+	public abstract void ruby(); 
+
+	
+}
